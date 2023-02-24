79b77a07274b2a3ea5a13cce5707a947746dbe82
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2023-02-23 23:38:57 -0300

    Input System Refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,34 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 7f;
     private bool isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        
-        if (Input.GetKey(KeyCode.W))
-            inputVector.y += 1;
-        
-        if (Input.GetKey(KeyCode.S))
-            inputVector.y -= 1;
-
-        if (Input.GetKey(KeyCode.A))
-            inputVector.x -= 1;
-        
-        if (Input.GetKey(KeyCode.D))
-            inputVector.x += 1;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
-        
+
         isWalking = moveDir != Vector3.zero;
+        const float rotateSpeed = 10f;
         
         // Update position
         transform.position += moveDir * (Time.deltaTime * moveSpeed);
-        
-        const float rotateSpeed = 10f;
-        
         // Update rotation
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
