7b86cb2fa96b25d834d67b84b8a52d17f6b55013
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2023-03-02 18:36:52 -0300

    Cutting Recipie SO

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipes;
     
     public override void Interact(Player player)
     {
         // There is no object on the counter, and player has one
-        if (!HasKitchenObject() && player.HasKitchenObject()) 
+        if (!HasKitchenObject() && player.HasKitchenObject() && HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
             // Place the player item on the counter
             player.GetKitchenObject().SetKitchenObjectParent(this);
         
@@ -20,13 +21,26 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        // There must be an object on the counter
-        if (!HasKitchenObject())
+        // There must be an object on the counter, and a valid recipe for cutting it
+        if (!HasKitchenObject() || !HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
             return;
         
-        // Cut the object
+        KitchenObjectSO cutKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+        
+        // Destroy the uncut object
         GetKitchenObject().DestroySelf();
             
+        // Spawn the cut object
         KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+    }
+    
+    private bool HasRecipeWithInput(KitchenObjectSO input)
+    {
+        return GetOutputForInput(input) != null;
+    }
+    
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO input)
+    {
+        return (from cuttingRecipe in cuttingRecipes where cuttingRecipe.input == input select cuttingRecipe.output).FirstOrDefault();
     }
 }
