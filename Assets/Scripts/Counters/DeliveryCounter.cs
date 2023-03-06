8a888b8df6fb5c37e255e8d11dc2360c7230fc75
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2023-03-05 23:27:52 -0300

    Delivery Manager

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -4,8 +4,10 @@ public class DeliveryCounter : BaseCounter
 {
     public override void Interact(Player player)
     {
-        if (!player.HasKitchenObject() || !player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plate))
+        if (!player.HasKitchenObject() || !player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             return;
+        
+        DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
         
         // Only accepts plates
         player.GetKitchenObject().DestroySelf();
---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : Singleton<DeliveryManager>
+{
+    [SerializeField] private RecipeListSO recipeListSO;
+
+    private List<RecipeSO> waitingRecipes;
+
+    private float spawnRecipeTimer;
+    
+    private const float spawnRecipeInterval = 4f;
+    private const int waitingRecipesMax = 4;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        waitingRecipes = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer += Time.deltaTime;
+
+        if (spawnRecipeTimer < spawnRecipeInterval) 
+            return;
+        
+        spawnRecipeTimer = 0f;
+            
+        if (waitingRecipes.Count >= waitingRecipesMax)
+            return;
+            
+        RecipeSO waitingRecipeSO = recipeListSO.recipes[Random.Range(0, recipeListSO.recipes.Count)];
+        waitingRecipes.Add(waitingRecipeSO);
+        
+        Debug.Log($"New recipe: {waitingRecipeSO.recipeName}");
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        foreach (RecipeSO waitingRecipe in waitingRecipes)
+        {
+            // The recipe cannot be the same if the number of ingredients is different
+            if (waitingRecipe.ingredients.Count != plateKitchenObject.GetKitchenObjectSOs().Count)
+                continue;
+
+            bool plateContentsMatchRecipe = true;
+            
+            // Cycle through all ingredients in the recipe
+            foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipe.ingredients)
+            {
+                bool ingredientFound = false;
+                
+                // Cycle through all ingredients on the plate
+                foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOs())
+                {
+                    if (plateKitchenObjectSO != recipeKitchenObjectSO) 
+                        continue;
+                    
+                    // Ingredient matches!!
+                    ingredientFound = true;
+                    break;
+                }
+
+                if (!ingredientFound)
+                {
+                    // This recipe ingredient was not found on the plate
+                    plateContentsMatchRecipe = false;
+                }
+            }
+
+            if (!plateContentsMatchRecipe)
+                continue;
+            
+            // The plate contents match the recipe, player delivered the correct recipe
+            Debug.Log($"Delivered CORRECT recipe: {waitingRecipe.recipeName}");
+            waitingRecipes.Remove(waitingRecipe);
+            return;
+        }
+        
+        // Player did not deliver a correct recipe
+        Debug.Log("Delivered INCORRECT recipe");
+    }
+}
